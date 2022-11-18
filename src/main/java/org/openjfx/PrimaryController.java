e29e321601a6787e8446b7bbf57641f01eedacbd
Baptiste Beau <baptiste.beau@ensea.fr>
2022-11-18 11:58:25 +0100

    chart graph internet

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,6 +1,9 @@
 package org.openjfx;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +11,29 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    MyChart myChart = new MyChart();
+
+    private void initialize() {
+        ChartUpdater server = new ChartUpdater(myChart);
+        server.setDaemon(true);
+        server.start();
+    }
+
+
+    @FXML
+    public static void addClient() throws IOException {
+        try {
+            Socket s = new Socket("10.10.26.33", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF(" 0.3 ");
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
